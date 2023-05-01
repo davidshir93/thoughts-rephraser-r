02add01ac4c76668508bf46fead54c09b2876c95
David Shir <davidshir93@gmail.com>
2023-05-01 20:43:49 +0300

    typo

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ if (
 	process.env.NODE_ENV === 'production' ||
 	process.env.NODE_ENV === 'staging'
 ) {
-	app.use(express.static(path.join(__dirname, 'client/builds')));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 	app.get('*', (req, res) => {
 		res.sendFile(path.join(__dirname + '/client/build/index.html'));
 	});
